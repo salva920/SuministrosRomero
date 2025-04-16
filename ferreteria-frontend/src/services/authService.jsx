bf20d749fbb22c10a354039513b63575e51470d1
salva920 <84866042+salva920@users.noreply.github.com>
2025-04-16 07:28:25 -0400

    Delete ferreteria-frontend directory
    asd

--- ferreteria-frontend/src/services/authService.jsx
+++
@@ -1,9 +0,0 @@
-export const logout = () => {
-  // Limpiar almacenamiento de forma síncrona
-  localStorage.removeItem('token');
-  sessionStorage.removeItem('isLoggedIn');
-  
-  // Forzar recarga completa sin usar navigate
-  window.location.href = '/';
-  window.location.reload();
-};
